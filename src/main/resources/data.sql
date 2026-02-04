6c8d75458b511986f4a76a7ed655549baa90d9cf
mjjyv <minhvueye363@gmail.com>
2026-02-04 22:01:41 +0700

    #5 small update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,9 @@
 -- Lưu ý: Nếu bảng của bạn tên là 'user', hãy để trong dấu ngoặc kép "user" vì 'user' là từ khóa hệ thống của PostgreSQL.
-INSERT INTO users (ten_dang_nhap, mat_khau, vai_tro) 
+INSERT INTO nguoi_dung (ten_dang_nhap, mat_khau, vai_tro) 
 VALUES ('20210001', '$2a$10$fuPNyWpqBaQvBSioWJ6EhOCjk2/vMVFTuaRGX73/Wjm5UqLlBPVrO', 'SINH_VIEN')
-ON CONFLICT (ten_dang_nhap) DO NOTHING;
 
-INSERT INTO users (ten_dang_nhap, mat_khau, vai_tro) 
+INSERT INTO nguoi_dung (ten_dang_nhap, mat_khau, vai_tro) 
 VALUES ('admin', '$2a$10$kGgO.8DNUHKJW9POxKqAIuQIc4S08fuGDCT51hkblwcSSTXeVQaTK', 'ADMIN')
-ON CONFLICT (ten_dang_nhap) DO NOTHING;
 
-INSERT INTO users (ten_dang_nhap, mat_khau, vai_tro) 
+INSERT INTO nguoi_dung (ten_dang_nhap, mat_khau, vai_tro) 
 VALUES ('giangvien01', '$2a$10$fuWnevp7Q5dfvdsxxrK6Jek9BMNuaKxOpH0cCXR85LNC0tLYLf0v2', 'GIANG_VIEN')
-ON CONFLICT (ten_dang_nhap) DO NOTHING;
